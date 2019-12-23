9b6d59b463594efd18e6eb4853aedce2ae984df5
John Victor Sampaio <johnvictorfs@gmail.com>
2019-12-23 14:52:44 -0300

    Add armour durability info to GameInfoHud

--- src/main/java/net/johnvictorfs/simple_utilities/hud/GameInfoHud.java
+++ src/main/java/net/johnvictorfs/simple_utilities/hud/GameInfoHud.java
@@ -1,5 +1,6 @@
 package net.johnvictorfs.simple_utilities.hud;
 
+import com.google.common.collect.Lists;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
@@ -7,14 +8,18 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Direction;
 
 import java.util.*;
+import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class GameInfoHud implements Drawable {
     private final MinecraftClient client;
     private final TextRenderer fontRenderer;
+    private final int lightGray = Integer.parseInt("696969", 16);
+    private final int white = 0x00E0E0E0;
     private Entity player;
 
     public GameInfoHud(MinecraftClient client) {
@@ -32,34 +37,37 @@ public class GameInfoHud implements Drawable {
 
     private void drawInfos() {
         // Draw lines of Array of Game info in the screen
-        int maxLineWidth = 10;
-        List<String> lines = getGameInfo();
 
-        for (String line : lines) {
-            maxLineWidth = Math.max(maxLineWidth, this.fontRenderer.getStringWidth(line));
-        }
+        List<String> gameInfo = getGameInfo();
+        drawArmourInfo();
 
-        int top = 0;
-
-        int lineHeight = this.fontRenderer.fontHeight + 2;
+        //int maxLineWidth = 10;
+        //for (String line : gameInfo) {
+        //    maxLineWidth = Math.max(maxLineWidth, this.fontRenderer.getStringWidth(line));
+        //}
         // maxLineWidth = (int) (Math.ceil(maxLineWidth / 5.0D + 0.5D) * 5);
         // int scaleWidth = this.client.getWindow().getScaledWidth();
         // int left = (scaleWidth - maxLineWidth) / 12 - 2;
+
+        int lineHeight = this.fontRenderer.fontHeight + 2;
+        int top = 0;
         int left = 4;
 
-        for (String line : lines) {
-            this.fontRenderer.draw(line, left, top + 4, 0x00E0E0E0);
+        for (String line : gameInfo) {
+            this.fontRenderer.draw(line, left, top + 4, lightGray);
             top += lineHeight;
         }
 
         if (this.player.isSprinting()) {
-            int maxLineHeight = Math.max(10, this.fontRenderer.getStringWidth("Sprinting"));
+            final String sprintingText = "Sprinting";
+
+            int maxLineHeight = Math.max(10, this.fontRenderer.getStringWidth(sprintingText));
             maxLineHeight = (int) (Math.ceil(maxLineHeight / 5.0D + 0.5D) * 5);
             int scaleHeight = this.client.getWindow().getScaledHeight();
             int sprintingTop = scaleHeight - maxLineHeight;
 
             // Sprinting Info
-            this.fontRenderer.draw("Sprinting", 2, sprintingTop + 20, 0x00E0E0E0);
+            this.fontRenderer.draw(sprintingText, 2, sprintingTop + 20, lightGray);
         }
     }
 
@@ -85,6 +93,38 @@ public class GameInfoHud implements Drawable {
         }
 
         return offset.trim();
+    }
+
+    private void drawArmourInfo() {
+        List<ItemStack> armourItems = new ArrayList<>();
+
+        int maxLineHeight = Math.max(10, this.fontRenderer.getStringWidth(""));
+
+        for (ItemStack armourItem : this.player.getArmorItems()) {
+            maxLineHeight = Math.max(maxLineHeight, this.fontRenderer.getStringWidth(I18n.translate(armourItem.getTranslationKey())));
+            armourItems.add(armourItem);
+        }
+
+        maxLineHeight = (int) (Math.ceil(maxLineHeight / 5.0D + 0.5D) * 5);
+        int armourTop = this.client.getWindow().getScaledHeight() - maxLineHeight;
+
+        int lineHeight = this.fontRenderer.fontHeight + 6;
+
+        // Draw in order Helmet -> Chestplate -> Leggings -> Boots
+        for (ItemStack armourItem : Lists.reverse(armourItems)) {
+            if (armourItem.getMaxDamage() == 0) {
+                // Skip empty slots
+                continue;
+            }
+
+            this.client.getItemRenderer().renderGuiItemIcon(armourItem, 2, armourTop - 54);
+
+            int currentDurability = armourItem.getMaxDamage() - armourItem.getDamage();
+            String itemDurability = currentDurability + "/" + armourItem.getMaxDamage();
+            this.fontRenderer.draw(itemDurability, 22, armourTop - 50, lightGray);
+
+            armourTop += lineHeight;
+        }
     }
 
     private List<String> getGameInfo() {
