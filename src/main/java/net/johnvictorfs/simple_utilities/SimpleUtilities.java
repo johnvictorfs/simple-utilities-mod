8dc6173c9ef40d541ef09780fa7c874572258e69
Spencer Burgess <40410913+SpencerBurgess@users.noreply.github.com>
2022-06-25 17:18:57 -0600

    Updated AutoConfig dependency
    AutoConfig is now bundled and maintained underneath ClothConfig, updated references to reflect this

--- src/main/java/net/johnvictorfs/simple_utilities/SimpleUtilities.java
+++ src/main/java/net/johnvictorfs/simple_utilities/SimpleUtilities.java
@@ -1,8 +1,8 @@
 package net.johnvictorfs.simple_utilities;
 
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.ConfigHolder;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.Toml4jConfigSerializer;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigHolder;
+import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
